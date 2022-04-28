457fd333458899affc587f48975af29d14926ff9
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-28 01:03:11 +0000

    fix: mypy error when `requests` not installed

--- eth_retry/conditional_imports.py
+++ eth_retry/conditional_imports.py
@@ -8,6 +8,6 @@ except ModuleNotFoundError:
     OperationalError = DummyException # type: ignore
 
 try:
-    from requests.exceptions import HTTPError, ReadTimeout
+    from requests.exceptions import HTTPError, ReadTimeout # type: ignore
 except ModuleNotFoundError:
     HTTPError, ReadTimeout = DummyException, DummyException # type: ignore
