ae1ebbe929eb90b349d02d611e524e9a6c63e9c3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-17 08:33:26 +0000

    fix(test): parallelize test_sync to  try toforce errs

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -2,10 +2,12 @@
 
 from brownie import web3
 from eth_retry.eth_retry import auto_retry
+from joblib import Parallel, delayed
+
 
 @auto_retry
 def do_work(w3):
-    work = [w3.eth.chain_id for i in range(5000)]
+    work = Parallel(100, 'threading')(delayed(getattr)(w3.eth, "chain_id") for i in range(5000))
     return work
 
 def test_auto_retry_sync():
