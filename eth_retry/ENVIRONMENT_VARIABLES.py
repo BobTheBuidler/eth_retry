4bef4f9490adb810c3c631608ff62013336605bc
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-01-10 22:33:21 -0500

    feat: ETH_RETRY_SUPPRESS_LOGS env var

--- eth_retry/ENVIRONMENT_VARIABLES.py
+++ eth_retry/ENVIRONMENT_VARIABLES.py
@@ -3,6 +3,8 @@ import os
 
 ETH_RETRY_DISABLED = bool(os.environ.get("ETH_RETRY_DISABLED"))
 ETH_RETRY_DEBUG = bool(os.environ.get("ETH_RETRY_DEBUG"))
-MIN_SLEEP_TIME = int(os.environ.get("MIN_SLEEP_TIME", 10))
-MAX_SLEEP_TIME = int(os.environ.get("MAX_SLEEP_TIME", 20))
+ETH_RETRY_SUPPRESS_LOGS = int(os.environ.get("ETH_RETRY_SUPPRESS_LOGS", 0))
+MIN_SLEEP_TIME = int(os.environ.get("MIN_SLEEP_TIME", 5))
+MAX_SLEEP_TIME = int(os.environ.get("MAX_SLEEP_TIME", 15))
 MAX_RETRIES = int(os.environ.get("MAX_RETRIES", 10))
+# NOTE: this will suppress logs up to `` times, then they will log as usual
