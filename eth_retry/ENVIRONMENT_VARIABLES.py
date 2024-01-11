1e6d61be011a4c5ded3aeb0ec72013ef15f4ea93
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-01-10 22:36:25 -0500

    chore: change default

--- eth_retry/ENVIRONMENT_VARIABLES.py
+++ eth_retry/ENVIRONMENT_VARIABLES.py
@@ -3,7 +3,7 @@ import os
 
 ETH_RETRY_DISABLED = bool(os.environ.get("ETH_RETRY_DISABLED"))
 ETH_RETRY_DEBUG = bool(os.environ.get("ETH_RETRY_DEBUG"))
-ETH_RETRY_SUPPRESS_LOGS = int(os.environ.get("ETH_RETRY_SUPPRESS_LOGS", 0))
+ETH_RETRY_SUPPRESS_LOGS = int(os.environ.get("ETH_RETRY_SUPPRESS_LOGS", -1))
 MIN_SLEEP_TIME = int(os.environ.get("MIN_SLEEP_TIME", 5))
 MAX_SLEEP_TIME = int(os.environ.get("MAX_SLEEP_TIME", 15))
 MAX_RETRIES = int(os.environ.get("MAX_RETRIES", 10))
