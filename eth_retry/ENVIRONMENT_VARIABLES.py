8a789ff94ccdaa241d00f7e51b2b11affd2c5cd2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-01-10 22:36:49 -0500

    Update ENVIRONMENT_VARIABLES.py

--- eth_retry/ENVIRONMENT_VARIABLES.py
+++ eth_retry/ENVIRONMENT_VARIABLES.py
@@ -3,8 +3,8 @@ import os
 
 ETH_RETRY_DISABLED = bool(os.environ.get("ETH_RETRY_DISABLED"))
 ETH_RETRY_DEBUG = bool(os.environ.get("ETH_RETRY_DEBUG"))
+# NOTE: this will suppress logs up to `ETH_RETRY_SUPPRESS_LOGS` times, then they will log as usual
 ETH_RETRY_SUPPRESS_LOGS = int(os.environ.get("ETH_RETRY_SUPPRESS_LOGS", -1))
 MIN_SLEEP_TIME = int(os.environ.get("MIN_SLEEP_TIME", 5))
 MAX_SLEEP_TIME = int(os.environ.get("MAX_SLEEP_TIME", 15))
 MAX_RETRIES = int(os.environ.get("MAX_RETRIES", 10))
-# NOTE: this will suppress logs up to `` times, then they will log as usual
