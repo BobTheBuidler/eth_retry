145f4dfe5f93f62bc0e53462dba03eb145d667a5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-02 20:03:37 -0400

    feat(test): test type error

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,5 +1,5 @@
 # type: ignore
-
+import pytest
 from brownie import web3
 from eth_retry.eth_retry import auto_retry
 from joblib import Parallel, delayed
@@ -12,3 +12,27 @@ def do_work(w3):
 
 def test_auto_retry_sync():
     do_work(web3)
+
+def test_max_retries_type_error():
+    with pytest.raises(TypeError):
+        @auto_retry(max_retries="3")
+        def fail():
+            ...
+
+def test_min_sleep_time_type_error():
+    with pytest.raises(TypeError):
+        @auto_retry(min_sleep_time="3")
+        def fail():
+            ...
+
+def test_max_sleep_time_type_error():
+    with pytest.raises(TypeError):
+        @auto_retry(max_sleep_time="3")
+        def fail():
+            ...
+
+def test_suppress_logs_type_error():
+    with pytest.raises(TypeError):
+        @auto_retry(suppress_logs="3")
+        def fail():
+            ...
